979f6e38063dfcbcabb1f407b22d7cba4d381ab7
Jim Irrer <irrer@umich.edu>
2015-09-01 14:39:03 -0400

    Make logging more robust.  If logging to a file fails, then log to the console instead so that the application still runs.

--- src/edu/umro/util/Log.java
+++ src/edu/umro/util/Log.java
@@ -2,13 +2,13 @@ package edu.umro.util;
 
 /*
  * Copyright 2012 Regents of the University of Michigan
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,19 +20,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.FileHandler;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import java.util.logging.SimpleFormatter;
 
 /**
- * Support logging services based on the <code>java.util.logging</code>
- * package.
+ * Support logging services based on the <code>java.util.logging</code> package.
  * <p>
- * In general, all logging is sent to files that are overwritten on a
- * rotating basis.
+ * In general, all logging is sent to files that are overwritten on a rotating basis.
  * 
- * @author Jim Irrer  irrer@umich.edu 
+ * @author Jim Irrer irrer@umich.edu
  *
  */
 public class Log {
@@ -41,11 +40,10 @@ public class Log {
     private volatile static Logger serviceLogger = null;
 
     /** Puts log messages into files. */
-    private volatile static FileHandler fileHandler = null;
+    private volatile static Handler handler = null;
 
     /** Formats log entries in a detailed way. */
     private volatile static LogFormatter serviceFormatter = null;
-
 
     /**
      * Get the service-wide logger.
@@ -57,22 +55,21 @@ public class Log {
         return serviceLogger;
     }
 
-
     /**
      * Set the logging level.
      * 
-     * @param level New logging level.
-     * @throws IOException 
-     * @throws SecurityException 
+     * @param level
+     *            New logging level.
+     * @throws IOException
+     * @throws SecurityException
      */
     public static void setLevel(Level level) throws SecurityException, IOException {
         initLogging();
-        fileHandler.setLevel(level);
+        handler.setLevel(level);
     }
 
-
     /**
-     * Determine if the logging is viable, meaning that it is ready for use.  This
+     * Determine if the logging is viable, meaning that it is ready for use. This
      * method is used in situations where logging may have not yet been initialized,
      * usually in the early startup phase of the program.
      * 
@@ -82,10 +79,9 @@ public class Log {
         return serviceLogger != null;
     }
 
-
     /**
      * Attempt to make any parent directories required by the file to
-     * be used for logging.  On failure, quietly give up.
+     * be used for logging. On failure, quietly give up.
      */
     private static void makeLogDir() {
         try {
@@ -102,48 +98,65 @@ public class Log {
             else {
                 System.out.println("Unable to read logging properties file: " + file.getAbsolutePath() + "  proceeding without logging.");
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             ;
         }
     }
-    
+
     /**
      * Format a <code>Throwable</code>.
      * 
-     * @param throwable Contains description and stack trace.
+     * @param throwable
+     *            Contains description and stack trace.
      * 
      * @return Human readable version of <code>Throwable</code> and stack trace.
      */
     public static String fmtEx(Throwable throwable) {
         StringBuffer buf = new StringBuffer(throwable.toString());
-        for (StackTraceElement ste : throwable.getStackTrace()) buf.append("\n    " + ste);
+        for (StackTraceElement ste : throwable.getStackTrace())
+            buf.append("\n    " + ste);
         return buf.toString();
     }
 
-
     /**
      * Initialize logging by setting up all loggers to use the same
-     * handler. 
+     * handler.
      * 
      */
     public static void initLogging() {
         if (serviceLogger == null) {
             try {
                 makeLogDir();
-                fileHandler = new LogFileHandler();
-                serviceLogger = Logger.getLogger(Log.class.getName());
+                Handler hndlr = new LogFileHandler();
+                Logger sl = Logger.getLogger(Log.class.getName());
                 serviceFormatter = new LogFormatter();
-                fileHandler.setFormatter(serviceFormatter);
-            }
-            catch (SecurityException ex) {
-                System.err.println("Security - Failed to set up logging to file: " + ex);
+                hndlr.setFormatter(serviceFormatter);
+
+                handler = hndlr;
+                serviceLogger = sl;
+            } catch (SecurityException ex) {
+                System.err.println("SecurityException - Failed to set up logging to file: " + ex);
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                System.err.println("IOException - Failed to set up logging to file: " + ex);
+                ex.printStackTrace();
+            } catch (Exception ex) {
+                System.err.println("Exception - Failed to set up logging to file: " + ex);
                 ex.printStackTrace();
             }
-            catch (IOException ex) {
-                System.err.println("IO - Failed to set up logging to file: " + ex);
-                ex.printStackTrace();
-            }
+        }
+
+        if (serviceLogger == null) {
+            System.err.println("Unable to log to file.  Logging to console instead.");
+            Logger logger = Logger.getLogger(Log.class.getName());
+            Handler hndlr = new ConsoleHandler();
+            hndlr.setFormatter(new SimpleFormatter());
+            logger.addHandler(hndlr);
+            logger.setLevel(Level.ALL);
+            hndlr.setLevel(Level.ALL);
+
+            handler = hndlr;
+            serviceLogger = logger;
         }
     }
 
